2baace4e70eb0b547997bb324d46f8505f4fda96
nataliadiaz2 <natdiaz2001@gmail.com>
2022-04-13 13:20:15 -0700

    Board working

--- src/App.js
+++ src/App.js
@@ -1,83 +1,66 @@
 import React, { Component } from 'react'
 import Square from './components/Square'
 import './App.css'
-import Board from './components/Board'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [null, null, null, null, null, null, null, null, null],      
-      playerOne: true,
-      
+      squares: [null, null, null, null, null, null, null, null, null],
+      playerOne: 'X',
+      win: null,
     }
   }
-
-
-
-
-startGame = (index) => {
-  const {squares} = this.state;
-    if(squares[index]){
-      return ;
-    }
-    squares[index] = this.state.playerOne ? 'X' : 'O'
-    this.setState({squares: squares, playerOne: !this.state.playerOne})
-  
-  
-}
-
-    calculateWinner = (squares,) => {
-        this.props.startGame(this.props.index)
-        const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }if (this.calculateWinner){
-          this.calculateWinner = 'Winner: ' + this.calculateWinner;
-        }else {
-          this.calculateWinner = 'Next player: ' + (this.state.playerOne)
-        }
-        }
-        return null;
+  startGame = (index) => {
+    const {squares, playerOne} = this.state;
+      if(squares[index]) {
+        return
+      } squares[index] = playerOne ? 'X' : 'O'
+      this.setState({squares: squares, playerOne: !playerOne});
+      this.calculateWinner()
     }
 
-
-
-
-
-
+calculateWinner = () => {
+    const winning = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+        winning.forEach(value => {
+          const [a, b, c] = value;
+          if (this.state.squares[a] === 'X' && this.state.squares[b] === 'X' && this.state.squares[c] === 'X') {
+            return this.setState({win: 'X'})
+          } else if (this.state.squares[a] === 'O' && this.state.squares[b] === 'O' && this.state.squares[c] === 'O') {
+            return this.setState({win: 'O'})
+        }
+      })
+    }
 
   render(){
-    
+    console.log(this.state.win)
+
     return(
       <div className='mainContainer'>
         <div className='container'>
           <h1>Tic Tac Toe</h1>
-      
+          
             <div className='ticTac'>
               {this.state.squares.map((value,index) =>
                 {return (
-                  <Square 
-                    key={index} 
+                  <Square
+                    key={index}
                     value={value}
                     index={index}
                     startGame = {this.startGame}
-              />
-              )
+                    />
+                  )
                 }
-                  )}
-            <Board startGame = {this.startGame}/>
+              )}
             </div>
         </div>
       </div>
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
 
 class Board extends Component {
-
-    calculateWinner = (squares) => {
-        this.props.startGame(this.props.index)
-        const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-        }
-        return null;
-    }
-
+  //    const winner = this.calculateWinner(this.state.squares);
+  // let status = 'Next player: ' + (this.state.playerOne ? 'X' : 'O');
+  // if (winner) {
+  //   status = 'Winner: ' + winner;
+  // } else {
+  //   status = 'Next player: ' + (this.props.playerOne ? 'X' : 'O');
+  // }
 
     render() {
 
